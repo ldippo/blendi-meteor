0a74d16b0d28532799e5482e14b677f468813840
Lyle Dippo <ldippo421@gmail.com>
2015-10-01 20:37:09 -0400

    added houston admin

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,7 @@
 Router.route('/', function () {
-
-
   this.layout('layout');
-
   this.render('ingredients', {to: "ingredients"});
   this.render('custom', {to: "custom"});
-
   this.render('products',
   {
     to: "products",
@@ -13,24 +9,16 @@ Router.route('/', function () {
       Session.set('category', this.params.name);
     }
   });
-
   this.render("cart", {to: "cart"});
-
   this.render("categories", {to: "categories"});
-
-
 });
 
-Router.route('/:name', function(){
+Router.route('/items/:name', function(){
   var params = this.params;
   var id = params.name;
-
   this.layout('layout');
-
   this.render('ingredients', {to: "ingredients"});
-
   this.render("cart", {to: "cart"});
-
   this.render("categories", {to: "categories"});
   this.render('products',
     {
